3696a2feb55b9eed44bb96ec0c4a0f72fcc0bc1e
quannguyen2306-ctrl <65434894+quannguyen2306-ctrl@users.noreply.github.com>
2022-06-23 11:45:00 +0700

    hahaha

--- wro/myapp/medicine/templates/medicine/app.py
+++ wro/myapp/medicine/templates/medicine/app.py
@@ -1,30 +1,65 @@
+from email.policy import default
 import json
-from unittest import result
+# from unittest import result
 from flask import request
 from flask import Flask, render_template
-from nbformat import write
-from numpy import result_type
+# from nbformat import write
+# from numpy import result_type
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///hospital.db'
+# Initialize the database
+db = SQLAlchemy(app)
+#Creating db models
+class Prescription(db.Model):
+    __tablename__ = "prescription"
+    id = db.Column(db.Integer, primary_key=True)
+    doctor_name = db.Column(db.String(200), nullable = False)
+    patient_number = db.Column(db.String(200), nullable = False)
+    medicine_list = db.Column(db.String(200), nullable = False)
+    amount_list = db.Column(db.String(200), nullable = False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    # Create function to return a string when we add something
+    def __repr__(self):
+        medicine = json.loads(self.medicine_list)
+        amount = json.loads(self.amount_list)
+        med_amt = []
+        for i in range(medicine):
+            med_amt.append((medicine[i],amount[i]))
+        return f"Doctor name: {self.doctor_name}, {med_amt}"
+class Medicine(db.Model):
+    __tablename__ = "medicine"
+    id = db.Column(db.Integer, primary_key=True)
+    medicine_name = db.Column(db.String(200), nullable = False)
+    description = db.Column(db.Text(), nullable = False)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+class Patient(db.Model):
+    __tablename__ = "patient"
+    id = db.Column(db.Integer, primary_key=True)
+    patient_name = db.Column(db.String(200), nullable = False)
+    disease_description = db.Column(db.Text(), nullable = False)
+
+def sendEv3JSON():
+    pass
+
 @app.route('/')
 def index():
     return render_template('./home.html')
-@app.route('/test', methods=['POST'])
-def test():
+@app.route('/input_prescription', methods=['POST'])
+def input_prescription():
     output = request.get_json()
-    print(output) # This is the output that was stored in the JSON within the browser
-    print(type(output))
-    result = json.loads(output) #this converts the json output to a python dictionary
-    print(result) # Printing the new dictionary
-    print(type(result))#this shows the json converted as a python dictionary
-    with open("data.json",'r+') as file:
-          # First we load existing data into a dict.
-        file_data = json.load(file)
-        # Join new_data with file_data inside emp_details
-        file_data["prescription"].append(result)
-        # Sets file's current position at offset.
-        file.seek(0)
-        # convert back to json.
-        json.dump(file_data, file, indent = 4)
+    result = json.loads(output) 
+    print(result)
+    pre = Prescription(
+        doctor_name = result["doctor_name"],
+        patient_number = result["patient_number"],
+        medicine_list = json.dumps(result["medicine"]),
+        amount_list = json.dumps(result["amount"]),    
+    )
+    db.session.add(pre)
+    db.session.commit()
     return result
 
 
